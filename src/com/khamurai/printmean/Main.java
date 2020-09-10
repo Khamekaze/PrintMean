21042aa0ed6626452746788a63957823d0dbb037
Kim Göransson <kim.goransson@student.kyh.se>
2020-09-10 14:50:36 +0200

    Added function for mean of 5 numbers

--- src/com/khamurai/printmean/Main.java
+++ src/com/khamurai/printmean/Main.java
@@ -7,11 +7,17 @@ public class Main {
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1, 2, 3, 4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1, -2, 3, -4));
         System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1, -11, 24, 2));
-        //System.out.println("Mean of -1, -11, 24, 2, 2000: " + meanCalc(-1, -11, 24, 2, 2000));
+        System.out.println("Mean of -1, -11, 24, 2, 2000: " + meanCalc(-1, -11, 24, 2, 2000));
     }
 
     public static String meanCalc(double a, double b, double c, double d) {
         double mean = (a + b + c + d) / 4.0;
+        String meanString = String.valueOf(mean);
+        return meanString;
+    }
+
+    public static String meanCalc(double a, double b, double c, double d, double e) {
+        double mean = (a + b + c + d + e) / 5.0;
         String meanString = String.valueOf(mean);
         return meanString;
     }
